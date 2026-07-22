2e01af3e68c3497ad2e1200352647ce98d9491fb
Sakshamtript-codes <sakshamtript.codes@gmail.com>
2026-07-22 21:41:01 +0530

    Learning pattern printing

---
+++ Patterns/floyds_triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int k=1;
+    cout<<"Enter the side length: ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+}
